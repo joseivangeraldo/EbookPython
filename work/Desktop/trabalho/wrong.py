c945ca4f15a65b885e8fd9a8032297de33c4a08c
José Ivan Geraldo da Silva <67489053+joseivangeraldo@users.noreply.github.com>
2023-09-04 08:43:57 +0000

    04-09-2023 08:43:54

--- work/Desktop/trabalho/wrong.py
+++ work/Desktop/trabalho/wrong.py
@@ -3,8 +3,8 @@ import graphics
 
 ##incorrect way to create two circles
 
-leftEye = Circle(50,50, 10)
+leftEye = Circle(Point(50,50), 10)
 leftEye.setFill('yellow')
 leftEye.setOutline('red')
-rightEye = LeftEye
+rightEye = leftEye
 rightEye.move(20, 0)
